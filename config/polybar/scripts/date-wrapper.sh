9b0a2d8d77a0132aa9b45fe466820a55a0d5c9c4
Brandon Lombano <brandonlombano@gmail.com>
2026-05-23 09:20:02 -0400

    fix: date-wrapper respeta idioma (es/en) usando active_lang.env

--- config/polybar/scripts/date-wrapper.sh
+++ config/polybar/scripts/date-wrapper.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+lang=$(cat "$HOME/.config/themes/lang/active_lang.env" 2>/dev/null | cut -d'"' -f2)
+[ -z "$lang" ] && lang="es"
+if [ "$lang" = "en" ]; then LOCALE="en_US.utf8"; else LOCALE="es_VE.utf8"; fi
+
 if [ -f /tmp/polybar-date-alt ]; then
-    LC_TIME=es_VE.utf8 date "+%A, %d %B %Y"
+    LC_TIME=$LOCALE date "+%A, %d %B %Y"
 else
     date "+%I:%M %p"
 fi
